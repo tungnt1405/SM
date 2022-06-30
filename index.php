46000c7f5cef10da4d724616a6cbb99e8b74ae86
tungnt1405 <ntt140520.bk@gmail.com>
2022-06-30 22:13:15 +0700

    base php

--- index.php
+++ index.php
@@ -8,24 +8,18 @@ session_start();
 * All data obtained will be transferred to index
 * This is the default page to display data
 */
-$uri =  $_SERVER['REQUEST_URI'];
-/*
--- đăng ký đường dẫn
--- sau khi đăng ký thì lấy uri về và gọi hàm so sánh với thằng uri nhập
--- nếu trùng thì return về trang controller ngược lại thì return lỗi
--- mặc định nếu không có giá trị truyền thì là trang chủ*/
 
-// if (isset($_GET['controller'])) {
-//     $controller = $_GET['controller'];
-//     if (isset($_GET['action'])) {
-//         $action = $_GET['action'];
-//     } else {
-//         $action = 'index';
-//     }
-// } else {
-//     $controller = 'pages';
-//     $action     =  'index';
-// }
+if (isset($_GET['controller'])) {
+    $controller = $_GET['controller'];
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+    } else {
+        $action = 'index';
+    }
+} else {
+    $controller = 'pages';
+    $action     =  'index';
+}
 
-// require_once("routers/base-route.php");
+require_once("routers/base-route.php");
 ?>
