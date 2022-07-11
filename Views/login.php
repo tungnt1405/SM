2d37acaa4fc0eb99d4e577ba95dc74f91ac1c8a7
tungnt1405 <ntt140520.bk@gmail.com>
2022-07-11 23:45:59 +0700

    reset init

--- Views/login.php
+++
@@ -1,5 +0,0 @@
-<form action="/login" method="post">
-    <input type="hidden" name="_method" value="PUT">
-    <input type="text" name="name" id="">
-    <button type="submit">Submit</button>
-</form>
